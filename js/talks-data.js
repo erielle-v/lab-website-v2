deb7a9ad7e4b1dba0091e20b970f14d47622a7c4
prabhakarlab <54130133+prabhakarlab@users.noreply.github.com>
2026-05-18 18:13:01 +0800

    TALKS

--- js/talks-data.js
+++ js/talks-data.js
@@ -1,89 +1,107 @@
 const talksData = [
     {
-        title: "BANKSY: A Spatial Omics Algorithm that Unifies Cell Type Clustering and Tissue Domain Segmentation",
-        event: "From Algorithms to Discovery in Genome-Scale Biology and Medicine",
-        date: "Jul 6, 2022",
-        youtube: "-6kP9RsReRM"
-    },
-    {
         title: "HCA Genetic Diversity Network (GDN), Asian Immune Diversity Atlas (AIDA)",
         event: "HCA Asia 2022 Meeting, Millennium Hilton Bangkok, Thailand",
         date: "Nov 3–4, 2022",
         youtube: "XAGj1JolGvQ"
     },
+
     {
         title: "Histone Acetylome-Wide Association Studies: Autism, TB, Antidepressants",
         event: "Neuroscience Singapore 2019 Symposium",
         date: "Oct 18, 2019",
         youtube: "IabLl2CalLg"
     },
+
+    {
+        title: "Introducing BANKSY: A New Spatial Omics Algorithm",
+        event: "STOC Workshop",
+        date: "Jul 22, 2022",
+        youtube: "qDNLcoIvqQc"
+    },
+
+    {
+        title: "BANKSY: A Spatial Omics Algorithm that Unifies Cell Type Clustering and Tissue Domain Segmentation",
+        event: "From Algorithms to Discovery in Genome-Scale Biology and Medicine",
+        date: "Jul 6, 2022",
+        youtube: "-6kP9RsReRM"
+    },
+
+    {
+        title: "HCA General Meeting 2022: Biological Networks Session",
+        event: "Human Cell Atlas General Meeting, Vienna, Austria",
+        date: "Jun 27, 2022",
+        youtube: "btEHmLj1bJI",
+        start: 1621
+    },
+
+    {
+        title: "CZI Ancestry Network Grant",
+        event: "CNA 938 Health Matters Interview, Singapore",
+        date: "Jan 26, 2022",
+        audio: "images/photo_album/CNA_938.mp3.ogg"
+    },
+
+    {
+        title: "Addressing Heterogeneity with Spatial Biology",
+        event: "10x Genomics Thought Leaders Series, Singapore",
+        date: "Jan 14, 2022",
+        youtube: "G9l933ifKQs"
+    },
+
     {
         title: "Single-cell data analytics: Asian immune diversity and cancer cell states",
         event: "19th International Conference on Bioinformatics (InCoB)",
         date: "Nov 25, 2020",
         youtube: "Y9eIy0SCt-s"
     },
-    {
-        title: "Introducing BANKSY: A New Spatial Omics Algorithm",
-        event: "STOC Workshop",
-        date: "2022",
-        youtube: "qDNLcoIvqQc"
-    },
-    {
-        title: "HCA General Meeting 2022 Day 2 — Biological Networks Session",
-        event: "Human Cell Atlas General Meeting, Vienna, Austria",
-        date: "Jun 27, 2022",
-        youtube: "btEHmLj1bJI",
-        start: 1621
-    },
-    {
-        title: "Health Matters: CZI Ancestry Network Grant",
-        event: "CNA 938 Health Matters Interview, Singapore",
-        date: "Jan 26, 2022",
-        audio: "images/photo_album/CNA_938.mp3.ogg"
-    },
-    {
-        title: "Thought Leaders Series — Addressing Heterogeneity with Spatial Biology",
-        event: "10x Genomics Thought Leaders Series, Singapore",
-        date: "Jan 14, 2022",
-        youtube: "G9l933ifKQs"
-    },
+
     {
         title: "Fast, Accurate and Scalable Algorithms for scRNA-seq and scATAC-seq",
         event: "Human Cell Atlas 3rd Annual Asia Meeting, Genome Institute of Singapore",
         date: "Nov 11, 2019",
+        link: "https://www.youtube.com/watch?v=hMeQC_ecLWs",
         youtube: "hMeQC_ecLWs"
     },
+
     {
         title: "Genetic and Geographical Diversity in the Human Cell Atlas",
         event: "Human Cell Atlas Equity Meeting, Addis Ababa, Ethiopia",
         date: "Oct 31, 2019",
+        link: "https://www.youtube.com/watch?v=qRNIbmtN3VU",
         youtube: "qRNIbmtN3VU"
     },
+
     {
         title: "Genetic Diversity",
         event: "Human Cell Atlas General Meeting, Barcelona, Spain",
         date: "Oct 11, 2019",
+        link: "https://www.youtube.com/watch?v=-OMSZTy_eLQ",
         youtube: "-OMSZTy_eLQ"
     },
+
     {
         title: "Histone Acetylation Changes in Autism",
         event: "Channel News Asia (CNA) Interview, Singapore",
         date: "Nov 18, 2018",
+        link: "https://drive.google.com/file/d/1Qeb2IpXWdSvVSQI6TvW2rHCPN-0kC97f/",
         drive: "1Qeb2IpXWdSvVSQI6TvW2rHCPN-0kC97f"
     },
+
     {
-        title: "Dr. Shyam Prabhakar on his Study on Autism",
+        title: "Dr. Shyam Prabhakar Talks About His Study on Autism",
         event: "The Straits Times Interview, Singapore",
         date: "Nov 17, 2016",
-        link: "https://www.youtube.com/watch?app=desktop&v=cKjcIs3Qq4k",
-        linkLabel: "Watch on YouTube"
+        link: "https://www.youtube.com/watch?v=cKjcIs3Qq4k",
+        youtube: "cKjcIs3Qq4k"
     },
+
     {
-        title: "Reference Component Analysis of Single-Cell Transcriptomes Reveals Cellular Heterogeneity in Colorectal Cancer",
-        event: "4th Annual Cancer Research & Oncology Virtual Conference",
+        title: "Reference Component Analysis of Single Cell Transcriptomes Reveals Cellular Heterogeneity in Colorectal Cancer",
+        event: "4th Annual Cancer Research & Oncology Week Virtual Event Series",
         date: "Oct 6, 2016",
         youtube: "lT8XlNGI1iM",
+        link: "https://www.youtube.com/watch?v=lT8XlNGI1iM",
         externalLink: "https://www.labroots.com/webinar/keynote-speaker-reference-component-analysis-of-single-cell-transcriptomes-reveals-cellular-heterogeneity-in-colorectal-cancer",
         externalLinkLabel: "Watch on Labroots"
     }
